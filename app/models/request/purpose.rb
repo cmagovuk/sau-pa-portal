d2c5706852f2b3ba2bdb4693bcb8abf8bdd06424
Chris Harvey <chris.harvey@deltascheme.com>
2022-12-20 08:38:35 +0000

    Changes to button text and service name.

--- app/models/request/purpose.rb
+++ app/models/request/purpose.rb
@@ -2,7 +2,7 @@ class Request::Purpose < Request
   PURPOSE_OPTIONS = %w[cultu emply energ envir infra regio rescu resea servi smesm train other].freeze
 
   # validate :atleast_one_selected
-  validates :other_purpose, presence: { if: ->(o) { o.purposes.include?("other") } }, length: { maximum: 255 }
+  validates :other_purpose, presence: { if: ->(o) { o.purposes.present? && o.purposes.include?("other") } }, length: { maximum: 255 }
 
   def atleast_one_selected
     errors.add(:purposes, "Select at least one purpose") unless PURPOSE_OPTIONS.any? { |a| purposes.include?(a.to_s) }
