2ad63633782f20cb2787445f7d5552652bd584cc
Chris Harvey <chris.harvey@deltascheme.com>
2024-07-29 09:19:19 +0100

    link validation

--- app/models/post_report/other_issues.rb
+++ app/models/post_report/other_issues.rb
@@ -1,5 +1,18 @@
 class PostReport::OtherIssues < PostReport
+ URI_REGEX = /\A#{URI::Parser.new.make_regexp(%w[https])}\z/
   validates :other_issues_text, length: { maximum: MAX_CHAR_COUNT }
+  validate :other_issues_link_format
+
+  def other_issues_link_format
+    return if other_issues_link.blank?
+    uri = as_uri(other_issues_link)
+
+    errors.add(:other_issues_link, :format) unless uri.present? && uri.scheme == "https" && uri.host.present?
+  end
+
+  def as_uri(value)
+    URI.parse(value.to_s) rescue nil if value.present?
+  end
 
   def permitted
     %w[other_issues other_issues_text other_issues_link].freeze
